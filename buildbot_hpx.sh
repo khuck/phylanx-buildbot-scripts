bb2391579f6d8e00ca6befa7f8b071917169b8d0
Kevin Huck <khuck@cs.uoregon.edu>
2019-02-07 09:21:56 -0800

    Freezing HPX to known good commit

--- buildbot_hpx.sh
+++ buildbot_hpx.sh
@@ -45,6 +45,7 @@ get_source()
     cd ${hpx_src_dir}
     git reset --hard
     git checkout master
+    git checkout 31e4a27da5e9c83ec9db901333d27174f893a07a
     git pull
     if [ -d apex ] ; then
         cd apex
