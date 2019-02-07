dd376cfec9c5dc204c842c8aa8243c91ef65ce5e
Kevin Huck <khuck@cs.uoregon.edu>
2019-02-07 09:42:35 -0800

    Got git command ordering wrong.

--- buildbot_hpx.sh
+++ buildbot_hpx.sh
@@ -45,8 +45,8 @@ get_source()
     cd ${hpx_src_dir}
     git reset --hard
     git checkout master
-    git checkout 31e4a27da5e9c83ec9db901333d27174f893a07a
     git pull
+    git checkout 31e4a27da5e9c83ec9db901333d27174f893a07a
     if [ -d apex ] ; then
         cd apex
         git fetch --tags
