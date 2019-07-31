0b3808f8ef101bf09a101513ad57a78c1c096d7e
Kevin Huck <khuck@cs.uoregon.edu>
2019-07-31 13:17:22 -0700

    Don't use HPX stable.  It's...unstable.

--- buildbot_hpx.sh
+++ buildbot_hpx.sh
@@ -48,8 +48,7 @@ get_source()
     git fetch --tags
     git checkout master
     git pull
-    git checkout stable
-    # git checkout 31e4a27da5e9c83ec9db901333d27174f893a07a
+    #git checkout stable
     if [ -d apex ] ; then
         cd apex
         git fetch --tags
