b20ea5f8b4d86caf7a808030eb867f52bba8c741
Kevin Huck <khuck@cs.uoregon.edu>
2020-01-24 10:47:52 -0800

    Adding a check for refresh to make sure it's in the right path

--- refresh.sh
+++ refresh.sh
@@ -5,8 +5,13 @@ if [ -z ${scriptdir} ] ; then
     scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 fi
 
-# First, update the Phylanx source
-cd ${scriptdir}/../..
+if [[ "${scriptdir}" != *phylanx/tools/buildbot ]] ; then
+    echo "This script should only be run from the phylanx/tools/buildbot directory."
+    kill -INT $$
+fi
+
+# First, update the Phylanx source - assume we are in phylanx/tools/buildbot
+cd ${scriptdir}/../../..
 cwd=`pwd`
 
-${scriptdir}/get_all_source.sh ${cwd}/..
+${scriptdir}/get_all_source.sh ${cwd}
